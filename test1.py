7ce5ea2c2863054f751357cd6488b7a1e958d94a
Gh-Is1998 <149622608+Gh-Is1998@users.noreply.github.com>
2026-03-08 06:37:19 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -12,17 +12,13 @@ df = df_clean.copy()
 
 df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
 
-# اختيار السنة من القائمة
+
 years = sorted(df['Date'].dt.year.dropna().unique())
 selected_year = st.selectbox("select the year", years)
 
-# فلترة حسب السنة
 filtered = df[df['Date'].dt.year == selected_year]
-
-# تحديد الألوان
 colors = np.where(filtered['Close'] > filtered['Open'], 'green', 'red')
 
-# رسم الحجم
 fig, ax = plt.subplots(figsize=(10,5))
 ax.bar(filtered['Date'], filtered['Volume'], color=colors)
 
